f95b9f52f667d1775e0ce29a50bd9f4792a0643d
Henry Mustapha <holyfireangel123@gmail.com>
2017-07-07 19:18:03 +0000

    still some work to do

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // JavaScript File
 // JavaScript File
 $( document ).ready(function() {
+    $("button").click(function(){
+        $("p2").append(img src="http://img.ulximg.com/image/405x405/artist/1393887878_8f90f0144ef59ba3585b860ffc7d503c.jpg/7559b8fad5aa7641a86b65da9b591668/1393887878_1610nie_60.jpg")
+    }
 
 
 });
